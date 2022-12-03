2953dee0742b1f49dbfd3dae7813ef3908de202f
junho <junho.ko@business-canvas.com>
2022-12-03 15:03:15 +0900

    add changeCharacterToValue

--- day3/solve.js
+++ day3/solve.js
@@ -4,6 +4,17 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const isLowerCase = (char) =>{
     return char === char.toLowerCase()
 }
-
 console.log(isLowerCase("A") === false)
 console.log(isLowerCase("a") === true)
+
+const changeCharToValue = (char)=>{
+    if(isLowerCase(char)){
+        return char.charCodeAt() - 96
+    } else{
+        return char.charCodeAt() - 38
+    }
+}
+console.log(changeCharToValue("a") === 1)
+console.log(changeCharToValue("z") === 26)
+console.log(changeCharToValue("A") === 27)
+console.log(changeCharToValue("Z") === 52)
