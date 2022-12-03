8d99e865b67edbf7399bd76980c7119354d496ce
junho <junho.ko@business-canvas.com>
2022-12-03 14:58:19 +0900

    add islowercase function

--- day3/solve.js
+++ day3/solve.js
@@ -1,3 +1,9 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+const isLowerCase = (char) =>{
+    return char === char.toLowerCase()
+}
+
+console.log(isLowerCase("A") === false)
+console.log(isLowerCase("a") === true)
