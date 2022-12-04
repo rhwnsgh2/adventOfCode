bea86eb398f65ff8be9f8fa9b84cd8830e63abdb
junho <junho.ko@business-canvas.com>
2022-12-04 14:38:14 +0900

    formatted input

--- day4/solve.js
+++ day4/solve.js
@@ -1,4 +1,17 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-console.log(input)
+const formatInput = ()=>{
+    const result = input.map((inputLine)=>{
+         return inputLine.split(',').map((item)=>{
+            const splitWithHyphen = item.split('-')
+            return { 
+                firstValue: splitWithHyphen[0],
+                lastValue :  splitWithHyphen[1],
+            }
+         })
+    })
+    return result
+} // [ [{first, last}, {first, last}] , ... ]
+
+formatInput()
