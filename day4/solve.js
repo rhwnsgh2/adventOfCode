28c78d029c677b3c240c58a2fccd1579661f13f6
junho <junho.ko@business-canvas.com>
2022-12-04 15:01:04 +0900

    change formatInput to sort

--- day4/solve.js
+++ day4/solve.js
@@ -1,22 +1,27 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+const sortWithLength = (list)=>{
+    return list.sort((a,b)=> (a.lastValue - a.firstValue) - (b.lastValue - b.firstValue))
+}
 const formatInput = ()=>{
     const result = input.map((inputLine)=>{
-         return inputLine.split(',').map((item)=>{
+        const formattedList = inputLine.split(',').map((item)=>{
             const splitWithHyphen = item.split('-')
             return { 
                 firstValue: Number(splitWithHyphen[0]),
                 lastValue :  Number(splitWithHyphen[1]),
             }
          })
+
+        const sortedList = sortWithLength(formattedList)
+        return sortedList
     })
     return result
-} // [ [{first, last}, {first, last}] , ... ]
+} // [ [{short, long}, {short, long}] , ... ]
 
 
 const formattedInput = formatInput()
 
-const sortWithLength = (list)=>{
-    return list.sort((a,b)=> (a.lastValue - a.firstValue) - (b.lastValue - b.firstValue))
-}
+
+console.log(formattedInput)
