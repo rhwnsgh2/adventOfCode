c811f9ef1770870847b0ed74f23671a530ff5b5a
junho <junho.ko@business-canvas.com>
2022-12-06 15:14:29 +0900

    add firstSubmit

--- day6/solve.js
+++ day6/solve.js
@@ -3,13 +3,6 @@ let input = fs.readFileSync('/dev/stdin').toString();
 
 const MARKER = 4;
 
-const firstSubmit = () =>{
-    let result = 0;
-    for(let i=0; i <input.length; i++){
-        const compareString = input.slice(i, i+MARKER);
-    }
-}
-
 const isUniqCharacterString = (inputString) =>{
     for(let i=0; i< inputString.length; i++){
         const sliceString = inputString.slice(i+1, inputString.length)
@@ -18,7 +11,21 @@ const isUniqCharacterString = (inputString) =>{
     return true;
 }
 
-console.log(isUniqCharacterString("aaaa"))
-console.log(isUniqCharacterString("abcd"))
-console.log(isUniqCharacterString("dadada"))
+console.log(isUniqCharacterString("aaaa") === false)
+console.log(isUniqCharacterString("abcd") === true)
+console.log(isUniqCharacterString("dadada") === false)
+
+const firstSubmit = () =>{
+    let result = 0;
+    for(let i=0; i <input.length; i++){
+        const compareString = input.slice(i, i+MARKER);
+        if(isUniqCharacterString(compareString)){
+            result = i + MARKER;
+            break;
+        } 
+    }
+    console.log(result)
+}
+
+
 firstSubmit()
