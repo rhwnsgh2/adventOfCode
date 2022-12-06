09b0f1a51917c6e280c3d3c00450f086206f830e
junho <junho.ko@business-canvas.com>
2022-12-06 15:12:45 +0900

    add isUniqCharacterString

--- day6/solve.js
+++ day6/solve.js
@@ -1,4 +1,24 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString();
 
-console.log(input)
+const MARKER = 4;
+
+const firstSubmit = () =>{
+    let result = 0;
+    for(let i=0; i <input.length; i++){
+        const compareString = input.slice(i, i+MARKER);
+    }
+}
+
+const isUniqCharacterString = (inputString) =>{
+    for(let i=0; i< inputString.length; i++){
+        const sliceString = inputString.slice(i+1, inputString.length)
+        if(sliceString.includes(inputString[i])) return false;
+    }
+    return true;
+}
+
+console.log(isUniqCharacterString("aaaa"))
+console.log(isUniqCharacterString("abcd"))
+console.log(isUniqCharacterString("dadada"))
+firstSubmit()
