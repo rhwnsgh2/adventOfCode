5a160d3dfebecd84d665a19adf24619a22402dee
junho <junho.ko@business-canvas.com>
2022-12-07 15:22:54 +0900

    fix parseInput to commandList

--- day7/solve.js
+++ day7/solve.js
@@ -4,24 +4,43 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const parseInput = (rawInput) => {
     const input = rawInput.slice()
 
+    const result = []
+    let lsResult = []
     input.forEach(line => {
         const splitWithWhiteSpace = line.split(' ')
         if(splitWithWhiteSpace[0] === '$'){
-            if(splitWithWhiteSpace[1] === 'cd') {
-                cdCommand()
-            }else{
-                lsCommand()
+            if(lsResult.length > 0){
+                result.push(lsResult);
+                lsResult = []
             }
+
+            if(splitWithWhiteSpace[1] === 'cd') {
+                result.push(cdCommand)
+            }else{
+                result.push(lsCommand)
+            }
+        }else if(splitWithWhiteSpace[0] === "dir"){
+            lsResult.push({
+                type : "dir",
+                name: splitWithWhiteSpace[1]
+            })
+        }else{
+            lsResult.push({
+                type:"file",
+                name:splitWithWhiteSpace[1],
+                size: Number(splitWithWhiteSpace[0])
+            })
         }
     })
+    return result
 }
 
-const cdCommand = ()=>{
-
+const cdCommand = (target)=>{
+    return target
 }
 
 const lsCommand = ()=>{
-    
+
 }
 
-parseInput(input)
+console.log(parseInput(input))
