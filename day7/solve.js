bfdc9589d593798fd5bc313bfd53bad8638cd778
junho <junho.ko@business-canvas.com>
2022-12-07 18:02:48 +0900

    add firstSubmit

--- day7/solve.js
+++ day7/solve.js
@@ -3,41 +3,47 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 const parseInputToCommandList = (rawInput) => {
     const input = rawInput.slice()
-
-    const result = []
+    let pathHistory = []
     let lsResult = []
+    let directory = {}
+
+    const pathHistoryToName = (pathHistory)=>{
+        const pathHistoryToText = pathHistory.join('/') + '/';
+        if(pathHistoryToText.slice(0,2) === '//') {
+            return pathHistoryToText.slice(1,pathHistoryToText.length)
+        }
+        return pathHistoryToText
+    }
+
     input.forEach((line, index) => {
         const splitWithWhiteSpace = line.split(' ')
         if(splitWithWhiteSpace[0] === '$'){
             if(lsResult.length > 0){
-                result.push({type: "lsResult", result : lsResult});
-                lsResult = []
+                directory[pathHistoryToName(pathHistory)] = lsResult
+                lsResult=[]
             }
 
             if(splitWithWhiteSpace[1] === 'cd') {
-                result.push({type:"cd", target : splitWithWhiteSpace[2]})
+                pathHistory = cdCommand(pathHistory, splitWithWhiteSpace[2])
             }
-            // else{
-            //     result.push({type:"ls"})
-            // }
         }else if(splitWithWhiteSpace[0] === "dir"){
             lsResult.push({
                 type : "dir",
-                name: splitWithWhiteSpace[1]
+                name: pathHistoryToName(pathHistory) + splitWithWhiteSpace[1] +'/'
             })
         }else{
             lsResult.push({
                 type:"file",
-                name:splitWithWhiteSpace[1],
+                name: splitWithWhiteSpace[1],
                 size: Number(splitWithWhiteSpace[0])
             })
         }
         if(input.length-1 === index){
-            result.push({type: "lsResult", result : lsResult});
+            directory[pathHistoryToName(pathHistory)] = lsResult
         }
     })
 
-    return result
+    return directory
 }
 
 
@@ -53,50 +59,29 @@ const cdCommand = (currentPath, target) =>{
     return path
 }
 
-const getCurrentPath = (pathHistory) => {
-    if(pathHistory.length === 0){
-        return '/'
-    }
-
-    return pathHistory[pathHistory.length - 1]
-}
-
-const createDirectoryWithCommandList = (commandList)=>{
-    let pathHistory = []
-    let directory = {}
-
-    commandList.forEach((command)=>{
-        if(command.type === 'cd'){
-            pathHistory = cdCommand(pathHistory, command.target)
-        }else if(command.type === 'lsResult'){
-            const currentPath = getCurrentPath(pathHistory)
-            directory[currentPath] = command.result
-        }
-    })
-
-    return directory
-}
-
-
 const main = ()=>{
-    const commandList = parseInputToCommandList(input)
-    const directories = createDirectoryWithCommandList(commandList)
-
+    const directories = parseInputToCommandList(input)
+    Object.keys(directories).forEach(key=>console.log(key))
     const getDirectoryFileSize = (directory) =>{
-        directory.reduce((prev,curr)=>{
+        console.log(directory)
+        return directory.reduce((prev,curr)=>{
             if(curr.type === "file"){
-                return prev + curr
+                return prev + curr.size
             }else if(curr.type === "dir"){
                 return prev + getDirectoryFileSize(directories[curr.name])
             }
         }, 0)
     }
 
-    const result = Object.keys(directories).reduce((prev,curr)=> {
-        return prev + getDirectoryFileSize(directories[curr])
+    const result = Object.keys(directories).reduce((prev,curr)=> { 
+        console.log(curr)
+        const totalSize = getDirectoryFileSize(directories[curr])
+        if(totalSize <= 100000) {
+            console.log(curr, totalSize)
+            return prev + totalSize
+        }
+        return prev 
     },0)
-
-    
 
     console.log(result)
 }
