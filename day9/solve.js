700cb8c82de2b269f1ff6645210a3ec3025118bd
junho <junho.ko@business-canvas.com>
2022-12-09 17:27:58 +0900

    add moveFunction

--- day9/solve.js
+++ day9/solve.js
@@ -1,18 +1,52 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const head = {x: 0, y:0}
-const tail = {x:0, y:0}
+const moveFunction = {
+    up: function(count){
+        this.y += count;
+    },
+    down: function(count){
+        this.y -= count;
+    },
+    left: function(count){
+        this.x -= count;
+    },
+    right: function(count){
+        this.x += count;
+    }}
+     
+const head = {x: 0, y:0, ...moveFunction}
+
+const tail = {x:0, y:0, ...moveFunction}
 
 const parseInput = (input)=>{
-    input.forEach((line)=>{
+    return input.map((line)=>{
         const direction = line.split(' ')[0]
-        const count = line.split(' ')[1]
-
-        command(direction, count)
+        const count = Number(line.split(' ')[1])
+        return {direction, count}
     })
 }
 
-const command = (direction, count)=>{
-    
+const move = (direction, count, target)=>{
+    switch(direction){
+        case "U":
+            target.up(count)
+            break;
+        case "R":
+            target.right(count)
+            break;
+        case "D":
+            target.down(count)
+            break;
+        case "L":
+            target.left(count)
+            break;
+    }
 }
+
+const firstResult = ()=>{
+    const parsedInput = parseInput(input)
+    parsedInput.forEach(({direction, count}) => move(direction, count, head))
+}
+
+firstResult()
