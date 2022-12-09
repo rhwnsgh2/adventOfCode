037e5ca5b06bed79642341a73e0928bdaab6a52c
junho <junho.ko@business-canvas.com>
2022-12-09 16:49:32 +0900

    add second submit

--- day8/solve.js
+++ day8/solve.js
@@ -64,4 +64,67 @@ const firstSubmit = ()=>{
 
 }
 
-firstSubmit()
+// firstSubmit()
+
+const countUntilEqualOrLarge = (number, list)=>{
+    let result = 0;
+    let i=0;
+    for(; i< list.length; i++){
+        if(list[i] >= number){
+            break;
+        }
+        result ++;
+    }
+
+    const slicedList = list.slice(i)
+
+    if(slicedList.length > 0){
+        let compareNumber = number-1; 
+        let li = [];
+        slicedList.forEach((item)=> {
+            if(item > compareNumber){
+                compareNumber = item;
+                li.push(item)
+            }
+        })
+        result += li.length
+    }
+    return result
+}
+
+const secondSubmit = ()=>{
+    const input = inputList
+
+    const resultList = emptyResultList
+
+     input.forEach((horizontalList, yIndex) => {
+        horizontalList.forEach((item,xIndex) => {
+            if(resultList[yIndex][xIndex] !== true){
+                const [horizontalListForward, horizontalListBehind] = splitListWithIndex(xIndex, horizontalList)
+                
+                const verticalList = [];
+                for(let i=0; i< input.length; i++){
+                    verticalList.push(input[i][xIndex])
+                }
+    
+                const [verticalListForward, verticalListBehind] = splitListWithIndex(yIndex, verticalList)
+                horizontalListForward.reverse()
+                verticalListForward.reverse()
+
+                const result = [horizontalListForward, horizontalListBehind, verticalListForward, verticalListBehind].reduce((prev, curr) =>{   
+                    if(curr.length === 0) return prev
+                    const functionResult = countUntilEqualOrLarge(item, curr)
+                    // console.log("test", item, curr, functionResult)
+                    return prev * functionResult
+                }, 1)
+
+                resultList[yIndex][xIndex] = result
+            }
+        })
+    })
+    console.log(resultList)
+    console.log(resultList.flat().sort((a,b)=>b-a))
+    // console.log(JSON.stringify(resultList) === JSON.stringify([[4,2,2,1],[2,1,2,1],[2,2,1,1]]))
+}
+
+secondSubmit()
