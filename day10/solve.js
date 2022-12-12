d9c3a773fc1988e057f886fc45351675570cc007
junho <junho.ko@business-canvas.com>
2022-12-12 16:00:05 +0900

    add firstSubmit

--- day10/solve.js
+++ day10/solve.js
@@ -8,13 +8,42 @@ const commandList = (target) =>{
     input.forEach((inputLine) => {
         const command = inputLine.split(' ')[0]
         if(command === "noop"){
-            result.push({command})
-        }else {
+            result.push({name : command})
+        }else if(command === "addx"){
             const variable = inputLine.split(' ')[1]
-            result.push({command, variable})
+            result.push({name: command, variable : Number(variable)})
         }
     })
     return result
 }
 
-console.log(commandList(input))
+const executeCommand = (command, cycle) => {
+    if(command.name === "addx"){
+        cycle.push((value)=> value)
+        cycle.push((value) =>{ return value + command.variable })
+    } else{
+        cycle.push((value)=> value)
+    }
+}
+
+const firstSubmit = ()=>{
+    const commands = commandList(input)
+
+    const cycle = [(value)=> value]
+    commands.forEach((command) => executeCommand(command, cycle))
+
+    let list = [20,60,100,140,180,220]
+    let answer = 0;
+
+    cycle.reduce((prev, curr, index)=> {
+        if(list.includes(index)) {
+            answer += index * prev
+        }
+        
+        return curr(prev)
+    },1)
+
+    console.log(answer)
+}
+
+firstSubmit()
